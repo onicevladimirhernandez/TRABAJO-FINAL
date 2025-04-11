077ba98239a7531f09ed1b29734928a95b55357b
onicevladimirhernandez <2023-0213@unad.edu.do>
2025-04-11 16:33:50 -0400

    se agrega el proyecto

---
+++ ProyectoFinaSERVER/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinaSERVER.Data;
+
+namespace ProyectoFinaSERVER.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }  
+    }
+}
